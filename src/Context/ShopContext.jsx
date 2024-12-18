b31b410e6bdb717b4308fbf119f19328c2fea716
vasi1993 <vasymoldovan93@gmail.com>
2024-12-18 14:12:12 +0200

    update

--- src/Context/ShopContext.jsx
+++ src/Context/ShopContext.jsx
@@ -18,30 +18,6 @@ const ShopContextProvider = (props) => {
     setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
   };
 
-  //   const getTotalCartAmount = () => {
-  //     let totalAmount = 0;
-  //     for(const item in cartItems)
-  //         {
-  //             if(cartItems[item] > 0)
-  //                 {
-  //                     let itemInfo = all_product.find(
-  //                         (product) => product.id === Number(item));
-  //                     totalAmount += itemInfo.new_price * cartItems[item]
-  //                 }
-  //         }
-  //         return totalAmount;
-  // }
-
-  //   const getTotalCartItems = () => {
-  //     let totalItem = 0;
-  //     for (const item in cartItems) {
-  //       if (cartItems[item] > 0) {
-  //         totalItem += cartItems[item];
-  //       }
-  //     }
-  //     return totalItem;
-  //   };
-
   const getTotalCartAmount = () => {
     let totalAmount = 0;
     for (const item in cartItems) {
